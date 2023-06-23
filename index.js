249d8882b11d909d02a405e293616c792d604d40
Uzair Ahmed <46860228+uzair-ahmed-01@users.noreply.github.com>
2023-06-23 13:13:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const name = document.getElementById("name");
+
+const tagsContainer = document.querySelector(".tags");
+
+const addName = () => {
+
+    if (name.value === "") {
+        return null;
+    }
+
+    const tag = document.createElement("div");
+
+    tag.setAttribute("Class", "tag");
+
+    tag.innerText = `Hey, I am ${name.value}`;
+
+    tagsContainer.append(tag);
+
+};
